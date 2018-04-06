6a4f82e60cd2f4aa54d03b3080bbe8b840082098
Sean Febo <seanfebo@techie.com>
2018-04-06 16:26:01 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -16,5 +16,5 @@ def take_a_number(katz_deli, person)
   puts "Welcome, #{person}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
-  if 
+def now_serving(katz_deli)
+  if katz_deli.length == 0 
