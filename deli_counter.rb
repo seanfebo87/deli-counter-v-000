0f5fc203c107c1d5f56d744c1364f6632769eb3a
Sean Febo <seanfebo@techie.com>
2018-04-06 15:42:59 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,12 +1,14 @@
 def line(people)
  if people.length == 0 
    return "The line is currently empty."
-  elsif people.length > 0 
+  else  
     current_line = "The line is currently:"
     people.each_with_index(1) do |person, index|
       current_line.push(" #{index}. #{person}")
     end 
-  return 
+  return current_line
+  end
+end
       
       
    
